d68a172a7a88b5f3a4cad82666337dd1582e92e9
carl <carl.hume@gmail.com>
2020-08-06 14:42:29 -0400

    Added Logging

--- src/main/java/com/tds/server/HealthCheckController.java
+++ src/main/java/com/tds/server/HealthCheckController.java
@@ -1,7 +1,7 @@
 package com.tds.server;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -11,10 +11,13 @@ public class HealthCheckController {
 
     private static final int STATUS_OK = 200;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger( HealthCheckController.class );
+
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping( "/health" )
     public HealthCheck report() {
+        LOGGER.info( "HealthCheckController.report() called" );
         return new HealthCheck( counter.incrementAndGet(), STATUS_OK );
     }
 
--- src/main/java/com/tds/server/SummerGameServer.java
+++ src/main/java/com/tds/server/SummerGameServer.java
@@ -1,11 +1,20 @@
 package com.tds.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SummerGameServer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger( HealthCheckController.class );
+
     public static void main( String [] args ) {
+        LOGGER.info( "Starting the SummerGameServer" );
         SpringApplication.run( SummerGameServer.class, args );
+
+        // TODO: Log HealthCheck ...
+        LOGGER.info( "SummerGameServer started successfully" );
     }
 }
