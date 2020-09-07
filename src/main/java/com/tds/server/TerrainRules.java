a869cd244c4b8d35d0dd67c0415d96c08ec0b238
carl <carl.hume@gmail.com>
2020-09-07 15:49:49 -0400

    Cannot define a terrain type more than once.

--- src/main/java/com/tds/server/TerrainRules.java
+++ src/main/java/com/tds/server/TerrainRules.java
@@ -22,7 +22,28 @@ public class TerrainRules {
     }
 
     public void addTerrain( Terrain terrain ) {
-        this.terrain.add( terrain );
+        if( alreadyDefinesTerrain( terrain ) == false ) {
+            this.terrain.add(terrain);
+        }
+    }
+
+    public boolean alreadyDefinesTerrain( Terrain terrain ) {
+        boolean alreadyDefined = true;
+        Terrain definition = findTerrainDefinitionForType( terrain.getType() );
+        if( definition.getType().equals( "NF" ) ) {
+            alreadyDefined = false;
+        }
+        return alreadyDefined;
+    }
+
+    public Terrain findTerrainDefinitionForType( String type ) {
+        Terrain foundTerrain = new Terrain( "NF", "Terrain Not Found" );
+        for( Terrain terrainDefinition : terrain ) {
+            if( terrainDefinition.getType().equals( type ) ) {
+                foundTerrain = terrainDefinition;
+            }
+        }
+        return foundTerrain;
     }
 
     public List<Terrain> getTerrain() {
--- src/test/java/com/tds/server/TerrainRulesTest.java
+++ src/test/java/com/tds/server/TerrainRulesTest.java
@@ -1,10 +1,20 @@
 package com.tds.server;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TerrainRulesTest {
+
+    private TerrainRules terrainRules;
+    private Terrain testTerrain;
+
+    @BeforeEach
+    public void setUp() {
+        this.terrainRules = new TerrainRules();
+        testTerrain = new Terrain( "test", "Test Terrain" );
+    }
 
     @Test
     public void testEmptyRulesHaveNoTerrain() {
@@ -16,5 +26,36 @@ public class TerrainRulesTest {
     public void testDefaultRulesHaveTwoTerrainTypes() {
         TerrainRules rules = TerrainRules.DEFAULT_RULES;
         assertEquals( 2, rules.countUniqueTerrainTypes() );
+    }
+
+    @Test
+    public void testThereIsOnlyOneDefinitionForEachTerrainType() {
+        Terrain testTerrain = new Terrain( "test", "Test Terrain" );
+        Terrain alsoTestTerrain = new Terrain( "test", "Also Test Terrain" );
+        terrainRules.addTerrain( testTerrain );
+        terrainRules.addTerrain( alsoTestTerrain );
+        assertEquals( 1, terrainRules.countUniqueTerrainTypes() );
+    }
+
+    @Test
+    public void testCanFindTerrainDefinitionByType() {
+        terrainRules.addTerrain( testTerrain );
+        assertEquals( testTerrain, terrainRules.findTerrainDefinitionForType( testTerrain.getType() ) );
+    }
+
+    @Test
+    public void testUndefinedTerrainReturnedIfCannotFindType() {
+        assertEquals( "NF", terrainRules.findTerrainDefinitionForType( "This is not a type" ).getType() );
+    }
+
+    @Test
+    public void testTerrainAlreadyDefined() {
+        terrainRules.addTerrain( testTerrain );
+        assertTrue( terrainRules.alreadyDefinesTerrain( testTerrain ) );
+    }
+
+    @Test
+    public void testTerrainNotDefined() {
+        assertFalse( terrainRules.alreadyDefinesTerrain( testTerrain ) );
     }
 }
